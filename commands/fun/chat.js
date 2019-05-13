eba1996bd8c70f7ee1096711a241e45adc5ebdc7
dalo2903 <dalo2903@gmail.com>
2019-05-13 11:17:23 +0700

    Add images

--- commands/fun/chat.js
+++ commands/fun/chat.js
@@ -1,14 +1,27 @@
 const commando = require('discord.js-commando');
 const {Wit, log} = require('node-wit');
+const fs = require('fs')
+const lines = require('../../lines.json');
 const {wit_ai_token} = require('../../get_config');
+
 const wit_client = new Wit({
     accessToken: wit_ai_token,
     logger: new log.Logger(log.DEBUG) // optional
 });
-const lines = require('../../lines.json');
-const intent_respond = {
-  text: ["calling","swear", "name_get", "greeting", "empty", "unknown"]
+
+var filename = "hinhtihon.txt"
+try{
+    var data = fs.readFileSync(filename, 'utf8')
+}catch(err){
+    console.log(err)
 }
+const images = data.split('\r\n')
+
+const intent_respond = {
+  text: ["calling","swear", "name_get", "greeting", "empty", "unknown"],
+  image: ["calling", "name_get"]
+}
+
 module.exports = class chatCommand extends commando.Command {
   constructor(client) {
       super(client, {
@@ -21,6 +34,7 @@ module.exports = class chatCommand extends commando.Command {
 
   async run(message, args) {
     var rep = "uk";
+    var intent = "unknown";
     if (!args.length){
       rep = this.getRandom(lines["empty"]);
     }
@@ -29,16 +43,20 @@ module.exports = class chatCommand extends commando.Command {
       //console.log(res);
       if(res.entities.intent)
       {
-        var intent = res.entities.intent[0].value
+        intent = res.entities.intent[0].value
         rep = this.getRandom(lines[intent]);
       }
       else
         rep = this.getRandom(lines["unknown"]);
     }
+    var options = {}
     if(intent_respond.text.indexOf(intent) >= 0){
       console.log(`Send text: ${rep}`)
-      message.reply(rep);
     }
+    if(intent_respond.image.indexOf(intent) >= 0){
+      options["files"]  = [this.getRandom(images)];
+    }
+    message.reply(rep, options)
   }
   getRandom(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
