1e0acdc61ce64351212122da714709964d7b5343
Doge le Shibe <doge@buppo.local>
2019-05-13 07:29:17 +0700

    Add replíe

--- commands/fun/chat.js
+++ commands/fun/chat.js
@@ -4,49 +4,39 @@ const {wit_ai_token} = require('../../config.json');
 const wit_client = new Wit({
     accessToken: wit_ai_token,
     logger: new log.Logger(log.DEBUG) // optional
-  });
+});
+const lines = require('../../lines.json');
 
-  module.exports = class cCommand extends commando.Command {
-    constructor(client) {
-        super(client, {
-            name: 'chat',
-            group: 'fun',
-            memberName: 'chat',
-            description: 'Meo meo.',
-        });
+module.exports = class chatCommand extends commando.Command {
+  constructor(client) {
+      super(client, {
+          name: 'chat',
+          group: 'fun',
+          memberName: 'chat',
+          description: 'Meo meo.',
+      });
+  }
+
+  async run(message, args) {
+    var rep = "uk";
+    if (!args.length){
+      rep = this.getRandom(lines["empty"]);
     }
-
-    async run(message, args) {
-        if (!args.length){
-            message.channel.send("Nói gì đi.");
-            return;
-        }
-        else{
-            var res = await wit_client.message(args , {})
-            console.log(res);
-            if(res.entities)
-            {
-              var intent = res.entities.intent[0].value
-              // console.log(intent);
-              if(intent === "greeting")
-                message.reply(`Chào @<${message.author.id }> :). `);
-              else if(intent === "name_get")
-                message.reply(`Mình tên là Nấm @<${message.author.id }> .<:bua:>`);
-              else
-                message.reply(`Uk`);
-            }
-            else
-              message.reply(`Uk`);
-
-            // const bua = client.emojis.find(emoji => emoji.name === "ayy");
-            //message.reply(`${ayy} LMAO`);
-
-            // res.then((data) => {
-            //         message.channel.send(data.entities)
-            //     })
-            //     .catch(console.error);
-            // }
-        }
+    else{
+      var res = await wit_client.message(args , {});
+      //console.log(res);
+      if(res.entities.intent)
+      {
+        var intent = res.entities.intent[0].value
+        rep = this.getRandom(lines[intent]);
+      }
+      else
+        rep = this.getRandom(lines["unknown"]);
     }
+    message.reply(rep);
+  }
+  getRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
 
 }
